155c78dacac58cdea38c9e1f053668f4fa8cae53
Kaziraiyhan <112329938+Kaziraiyhan@users.noreply.github.com>
2023-03-23 13:14:09 +0600

    brack time

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('.review_slaider').slick({
+        centerMode: true,
+         centerPadding: '0px',
+         slidesToShow: 3,
+         slidesToScroll: 1,
+         prevArrow : ".prev_arrow",
+         nextArrow : ".next_arrow",
+         dots: true
+
+    });
+
+
+  });
